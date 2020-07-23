27624a9b64fb3c2b54cd91435687930d0427de1d
yamini1987 <67826757+yamini1987@users.noreply.github.com>
2020-07-23 20:31:22 +0530

    Add files via upload

---
+++ Day4_assignment.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+str4="What we think we become we are python programmer"
+str4.find("we")
+
+
+# In[2]:
+
+
+str4.rfind("we")
+
+
+# In[3]:
+
+
+print(str4.rindex("we")-10)
+
+
+# In[4]:
+
+
+str3="AbcD"
+str3.islower()
+
+
+# In[5]:
+
+
+str3="ABC"
+str3.isupper()
+
+
+# In[ ]:
+
+
+
+
